b8e401776332100658949beada2e4763a7f9a52e
niteeshbhat <niteeshbhathm@gmail.com>
2018-02-09 16:58:04 +0100

    Function for common validation for both profiles

--- webfe/CrossValidation_HbbTV_DVB.php
+++ webfe/CrossValidation_HbbTV_DVB.php
@@ -7,7 +7,7 @@
  */
  function CrossValidation_HbbTV_DVB_Segments($hbbtv,$dvb)
  {
-    
+    common_crossValidation();
     
      if($hbbtv)
          crossValidation_HbbTV_Segments();
@@ -15,4 +15,34 @@
      if($dvb)
          crossValidation_DVB_Segments();
 
+     
  }
+ 
+ function common_crossValidation()
+ {
+     global $locate, $Period_arr;
+    
+    for($adapt_count=0; $adapt_count<sizeof($Period_arr); $adapt_count++){
+        $loc = $locate . '/Adapt' . $adapt_count.'/';
+        
+        $Adapt=$Period_arr[$adapt_count];
+        $filecount = 0;
+        $files = glob($loc . "*.xml");
+        if($files)
+            $filecount = count($files);
+        
+        if(!($opfile = fopen($locate."/Adapt".$adapt_count."_compInfo.txt", 'a'))){
+            echo "Error opening/creating HbbTV/DVB Cross representation validation file: "."./Adapt".$adapt_count."_compInfo.txt";
+            return;
+        }
+        
+        if(file_exists($loc)){
+       
+        }
+
+        fprintf($opfile, "\n-----Conformance checks completed----- ");
+        fclose($opfile);
+    }
+ }
+ 
+ 
