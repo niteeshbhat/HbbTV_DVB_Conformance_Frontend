2d0fc8551754b5fc56524f234b59c35386a50851
eceozturk <oeztuerk@nomor.de>
2018-04-25 16:55:23 +0200

    DVB Metric reporting mechanism check

--- webfe/MPD_HbbTV_DVB.php
+++ webfe/MPD_HbbTV_DVB.php
@@ -291,6 +291,33 @@ function DVB_mpdvalidator($dom, $mpdreport){
     }
     ##
     
+    ## Verifying the DVB Metric reporting mechanism according to Section 10.12.3
+    $metrics = $MPD->getElementsByTagName('Metrics');
+    if($metrics->length != 0){
+        foreach($metrics as $metric){
+            $reportings = $metric->getElementsByTagName('Reporting');
+            if($reportings->length != 0){
+                foreach($reportings as $reporting){
+                    if($reporting->getAttribute('schemeIdUri') == 'urn:dvb:dash:reporting:2014' && $reporting->getAttribute('value') == 1){
+                        if($reporting->getAttribute('reportingUrl') == '')
+                            fwrite($mpdreport, "Information on DVB conformance: Section 10.12.3 - Where DVB Metric reporting mechanism is indicated in a Reporting descriptor, it SHALL have the @reportingUrl attribute.\n");
+                        else{
+                            if(!isAbsoluteURL($reporting->getAttribute('reportingUrl')))
+                                fwrite($mpdreport, "Information on DVB conformance: Section 10.12.3 - value of the @reportingUrl attribute in the Reporting descriptor needs to be and absolute HTTP or HTTPS URL.\n");
+                        }
+                        
+                        if($reporting->getAttribute('probability') != ''){
+                            $probability = $reporting->getAttribute('probability');
+                            if(!(((string) (int) $probability === $probability) && ($probability <= 1000) && ($probability >= 0)))
+                                fwrite($mpdreport, "Information on DVB conformance: Section 10.12.3 - value of the @probability attribute in the Reporting descriptor needs to be a positive integer between 0 and 1000.\n");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    ##
+    
     $cenc = $MPD->getAttribute('xmlns:cenc');
     
     // Periods within MPD
